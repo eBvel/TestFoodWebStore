e1f617baf88c148a2c36efa720b2299290529536
eBvel <belyakov041098@gmail.com>
2026-04-15 13:57:11 +0700

    Added get method

--- utils/http_methods.py
+++ utils/http_methods.py
@@ -7,6 +7,13 @@ class CustomRequests:
         self.headers = {"Content-type": "application/json"}
         self.cookie = {}
 
+    def get(self, url):
+        return self.session.get(
+            url=url,
+            headers=self.headers,
+            cookies=self.cookie
+        )
+
     def post(self, url, body):
         return self.session.post(
             url,
