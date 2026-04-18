11b00c9fd3aed1f932c3f559e947142308bf867b
eBvel <belyakov041098@gmail.com>
2026-04-18 12:37:30 +0700

    Created admin_token and user_token fields. Added get_product_id_list method. Added auth methods: by_admin and by_user.

--- webstore_config/webstore_api.py
+++ webstore_config/webstore_api.py
@@ -5,12 +5,17 @@ class WebstoreAPI:
     def __init__(self, session):
         self.request = CustomRequests(session)
         self.base_url = "http://91.197.96.80:5267/api/v1/"
+
+        self.admin_token = None
+        self.user_token = None
+
         self.auth_end_point = f"{self.base_url}authorization/login"
         self.create_product_end_point = f"{self.base_url}storage/product"
         self.delete_product_end_point = f"{self.base_url}storage/product?id="
-        self.cart_end_point = f"{self.base_url}cart/product"
+        self.cart_products_end_point = f"{self.base_url}cart/products"
+        self.cart_product_end_point = f"{self.base_url}cart/product"
 
-    def auth_by_admin(self, login, password):
+    def auth(self, login, password):
         response = self.request.post(
             url=self.auth_end_point,
             body={
@@ -18,9 +23,24 @@ class WebstoreAPI:
                 "password": password
             }
         )
-        self.request.set_auth_token(response.json().get('data').get('token'))
-        return response.status_code == 200
+        is_auth = response.status_code == 200
 
+        if is_auth and login is 'admin':
+            self.admin_token = response.json().get('data').get('token')
+        elif is_auth:
+            self.user_token = response.json().get('data').get('token')
+
+        return is_auth
+
+    def by_admin(self):
+        if self.admin_token is not None:
+            self.request.set_auth_token(self.admin_token)
+        return self
+
+    def by_user(self):
+        if self.user_token is not None:
+            self.request.set_auth_token(self.user_token)
+        return self
 
     def create_product(self, body_json):
         response = self.request.post(
@@ -35,9 +55,13 @@ class WebstoreAPI:
         )
         return response.status_code == 200
 
+    def get_product_id_list(self):
+        response = self.request.get(self.cart_products_end_point)
+        return response.json().get('data')
+
     def add_to_cart(self, product_id, count):
         response = self.request.post(
-            url=self.cart_end_point,
+            url=self.cart_product_end_point,
             body={
                 "productId": product_id,
                 "quantity": count
@@ -47,7 +71,7 @@ class WebstoreAPI:
 
     def remove_from_cart(self, product_id, count):
         response = self.request.delete(
-            url=self.cart_end_point,
+            url=self.cart_product_end_point,
             body={
                 "productId": product_id,
                 "quantity": count
