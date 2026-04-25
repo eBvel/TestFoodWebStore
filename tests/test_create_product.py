b3a5276a5d1d0c9df6d3c14374d17ce3edb4b2f2
eBvel <belyakov041098@gmail.com>
2026-04-25 13:35:22 +0700

    Combined the tests using parametrization

--- tests/test_create_product.py
+++ tests/test_create_product.py
@@ -1,181 +1,71 @@
 import allure
 
+from pytest import mark
 from pages.create_product_page import CreateProductPage
 from tests.test_data import headers, product_data
 from pages.edit_products_page import EditProductsPage
+from tests.test_data.test_products import ProductFactory
 
 
 class TestCreateProductPage:
-
     @classmethod
     def setup_class(cls):
         cls.edit_products = EditProductsPage(cls.driver)
         cls.create_product = CreateProductPage(cls.driver)
 
-    def _filling_product_data(
-            self,
-            name,
-            description,
-            category,
-            price,
-            image_url
-    ):
-        self.create_product.enter_product_name(name)
-        self.create_product.enter_product_description(description)
-        self.create_product.enter_expected_category(category)
-        self.create_product.enter_price_of_product(price)
-        self.create_product.enter_image_source(image_url)
-
     @allure.feature('CREATE PRODUCT')
     @allure.story('Проверка создания товара с корректными данными')
-    def test_create_product(self, auth_by_admin):
-        self.edit_products.open()
-
-        #Если товар, который добавляем, уже существует - удаляем его
-        if self.edit_products.product_is_exists(product_data.PEPPERONI_NAME):
-            self.edit_products.click_delete_product_button(
-                product_data.PEPPERONI_NAME
-            )
-
+    @mark.parametrize(
+        'product',
+        [ProductFactory.pepperoni(), ProductFactory.pepperoni(price=0)],
+        ids=['custom product', 'product with zero price']
+    )
+    def test_create_product(self, product, auth_by_admin, delete_new_products):
         self.create_product.open()
-        self._filling_product_data(
-            product_data.PEPPERONI_NAME,
-            product_data.PEPPERONI_DESCRIPTION,
-            product_data.PEPPERONI_CATEGORY,
-            product_data.PEPPERONI_PRICE_INT,
-            product_data.PEPPERONI_IMAGE_URL
-        )
+        self.create_product.filling_fields(product)
         self.create_product.click_create_product_button()
 
-        #Проверяем, что вернулись на страницу "Редактирование товаров"
+        #EXPECTED_VALUE
         self.edit_products.check_url()
         self.edit_products.check_header(headers.EDIT_PRODUCTS_PAGE)
-        #Проверяем наличие товара в каталоге
-        self.edit_products.check_product_is_exists(
-            product_data.PEPPERONI_NAME,
+        self.edit_products.check_existence_of_product(
+            product.name,
             True
         )
-
-    @allure.feature('INCORRECT CREATE PRODUCT')
-    @allure.story('Проверка создания товара с пустым полем "Имя"')
-    def test_create_product_without_name(self, auth_by_admin):
-        self.create_product.open()
-
-        self._filling_product_data(
-            '',
-            product_data.PEPPERONI_DESCRIPTION,
-            product_data.PEPPERONI_CATEGORY,
-            product_data.PEPPERONI_PRICE_INT,
-            product_data.PEPPERONI_IMAGE_URL
-        )
-
-        self.create_product.check_field_border_color(
-            'name',
-            'rgb(220, 53, 69)'
-        )
-        self.create_product.check_create_button_is_enabled(False)
-
-    @allure.feature('INCORRECT CREATE PRODUCT')
-    @allure.story('Проверка создания товара с пустым полем "Описание"')
-    def test_create_product_without_description(self, auth_by_admin):
-        self.create_product.open()
-
-        self._filling_product_data(
-            product_data.PEPPERONI_NAME,
-            '',
-            product_data.PEPPERONI_CATEGORY,
-            product_data.PEPPERONI_PRICE_INT,
-            product_data.PEPPERONI_IMAGE_URL
-        )
-
-        self.create_product.check_field_border_color(
-            'description',
-            'rgb(220, 53, 69)'
-        )
-        self.create_product.check_create_button_is_enabled(False)
-
-    @allure.feature('INCORRECT CREATE PRODUCT')
-    @allure.story('Проверка создания товара с пустым полем "URL картинки"')
-    def test_create_product_without_image_url(self, auth_by_admin):
-        self.create_product.open()
-
-        self._filling_product_data(
-            product_data.PEPPERONI_NAME,
-            product_data.PEPPERONI_DESCRIPTION,
-            product_data.PEPPERONI_CATEGORY,
-            product_data.PEPPERONI_PRICE_INT,
-            ''
-        )
-
-        self.create_product.check_field_border_color(
-            'image_url',
-            'rgb(220, 53, 69)'
-        )
-        self.create_product.check_create_button_is_enabled(False)
 
     @allure.feature('INCORRECT CREATE PRODUCT')
     @allure.story('Проверка создания товара с пустым полем "Цена"')
-    def test_create_product_without_price(self, auth_by_admin):
+    @mark.parametrize(
+        'field_name, item',
+        [
+            ('name', ProductFactory.pepperoni(name='')),
+            ('description', ProductFactory.pepperoni(description='')),
+            ('price', ProductFactory.pepperoni(price=None)),
+            ('price', ProductFactory.pepperoni(price=-1)),
+            ('image_url', ProductFactory.pepperoni(image_url=''))
+        ],
+        ids=[
+            'empty name',
+            'empty description',
+            'None price',
+            'negative price',
+            'empty image_url'
+        ]
+    )
+    def test_create_product_with_negative_data(
+            self,
+            field_name,
+            item,
+            auth_by_admin
+    ):
         self.create_product.open()
-
-        self._filling_product_data(
-            product_data.PEPPERONI_NAME,
-            product_data.PEPPERONI_DESCRIPTION,
-            product_data.PEPPERONI_CATEGORY,
-            '',
-            product_data.PEPPERONI_IMAGE_URL
-        )
+        self.create_product.filling_fields(item)
 
         self.create_product.check_field_border_color(
-            'price',
+            field_name,
             'rgb(220, 53, 69)'
         )
-        self.create_product.check_create_button_is_enabled(False)
-
-    @allure.feature('CREATE PRODUCT')
-    @allure.story('Проверка создания товара с нулевой ценой')
-    def test_create_product_with_zero_price(self, auth_by_admin):
-        self.edit_products.open()
-
-        if self.edit_products.product_is_exists(product_data.MARGARITA_NAME):
-            self.edit_products.click_delete_product_button(
-                product_data.MARGARITA_NAME
-            )
-
-        self.create_product.open()
-        self._filling_product_data(
-            product_data.MARGARITA_NAME,
-            product_data.MARGARITA_DESCRIPTION,
-            product_data.MARGARITA_CATEGORY,
-            0,
-            product_data.MARGARITA_IMAGE_URL
-        )
-        self.create_product.click_create_product_button()
-
-        self.edit_products.check_url()
-        self.edit_products.check_header(headers.EDIT_PRODUCTS_PAGE)
-        self.edit_products.check_product_is_exists(
-            product_data.MARGARITA_NAME,
-            True
-        )
-
-    @allure.feature('INCORRECT CREATE PRODUCT')
-    @allure.story('Проверка создания товара с отрицательной ценой')
-    def test_create_product_with_negative_price(self, auth_by_admin):
-        self.create_product.open()
-
-        self._filling_product_data(
-            product_data.MARGARITA_NAME,
-            product_data.MARGARITA_DESCRIPTION,
-            product_data.MARGARITA_CATEGORY,
-            -10,
-            product_data.MARGARITA_IMAGE_URL
-        )
-
-        self.create_product.check_field_border_color(
-            'price',
-            'rgb(220, 53, 69)'
-        )
+        #EXPECTED_VALUE
         self.create_product.check_create_button_is_enabled(False)
 
     @allure.feature('NAVIGATION')
