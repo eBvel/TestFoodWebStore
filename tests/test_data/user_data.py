19949c8846a276aacb96cbaffd041eb297cc273a
eBvel <belyakov041098@gmail.com>
2026-05-06 11:56:44 +0700

    Reversed the value of the: first name and second name.

--- tests/test_data/user_data.py
+++ tests/test_data/user_data.py
@@ -3,8 +3,8 @@ from dataclasses import dataclass
 
 @dataclass
 class UserData:
-    FIRST_NAME = "Блок"
-    SECOND_NAME = "Борис"
+    FIRST_NAME = "Борис"
+    SECOND_NAME = "Блок"
     MIDDLE_NAME = "Богданович"
     ADDRESS = "Омск, ул. Ленина, дом. 15, кв. 10"
     CART_NUMBER = "2202 2002 5667 9324"
